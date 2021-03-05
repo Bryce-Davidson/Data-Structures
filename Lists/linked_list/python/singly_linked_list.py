7c75516d9fc81f1a49165dd13ff4e74e57ea2efb
Bryce-Davidson <lifebryce@gmail.com>
2021-03-04 21:19:46 -0800

    Added indexing, finalized wikipedia specifications

--- Lists/linked_list/python/singly_linked_list.py
+++ Lists/linked_list/python/singly_linked_list.py
@@ -31,11 +31,23 @@ class SinglyLinkedList:
         newNode.next = self.head
         self.head = newNode
 
+    def find_by_value(self, value):
+        if self.is_empty():
+            raise KeyError(f'Cannot key empty list.')
+        curNode = self.head
+        while curNode:
+            if curNode.data == value:
+                return curNode.data
+            curNode = curNode.next
+        raise KeyError("Cannot find {value} in list.")
+
+    def update_by_index(self):
+        return
+
     def __getitem__(self, i):  
         if self.is_empty():
             raise IndexError(f'Cannot index empty list.')
         curNode = self.head
-        
         idx = 0
         while curNode:
             if idx == i:
@@ -44,14 +56,19 @@ class SinglyLinkedList:
             idx += 1
         raise IndexError("Index is out of bounds.")
 
-    def find_by_value(self):
-        return
-
-    def update_by_index(self):
-        return
-
-    def update_by_value(self):
-        return
+    def __setitem__(self, i, value):
+        if self.is_empty():
+            raise IndexError(f'Cannot set item for empty list.')
+        curNode = self.head
+        idx = 0
+        while curNode:
+            if idx == i:
+                curNode.data = value
+                return
+            curNode = curNode.next
+            idx += 1
+        raise IndexError("Index is out of bounds.")
+        
 
     def __str__(self):
         s = "["
@@ -64,9 +81,10 @@ class SinglyLinkedList:
         s += "]"
         return s
 
+# ---------------------------------------------------------------------------
 
 def print_test(truth_value, test_description):
-    s = "Test [{:^14}]: ".format(test_description)
+    s = "Test [{:^30}]: ".format(test_description)
     if truth_value:
         s += "PASS"
     else:
@@ -118,6 +136,48 @@ def get_by_index_test():
     value = sll[1]
     print_test(value == 2, "get by index")
 
+def get_by_index_ERROR_test():
+    sll = SinglyLinkedList()
+    sll.append(1)
+    sll.append(2)
+    sll.append(3)
+    try:
+        value = sll[4]
+    except IndexError:
+        print_test(True, "get by index: IndexError")
+        return
+    print_test(False, "get by index: IndexError")
+
+
+def set_by_index_test():
+    sll = SinglyLinkedList()
+    sll.append(1)
+    sll.append(2)
+    sll.append(3)
+    sll[1] = 42
+    print_test(sll[1] == 42, "set by index")
+
+def find_by_value_test():
+
+    return
+
+def update_by_value_test():
+    return
+
+def remove_at_beggining_test():
+    return
+
+def remove_at_end_test():
+    return
+
+def remove_at_index_test():
+    return
+
+def remove_by_value_test():
+    return
+
+def reversal_of_list_test():
+    return
 
 def main():
     constructor_test()
@@ -127,6 +187,20 @@ def main():
     get_head_test()
     # get_tail_test()
     get_by_index_test()
+    
+    get_by_index_ERROR_test()
+
+    set_by_index_test()
+    find_by_value_test()
+    update_by_value_test()
+
+    remove_at_beggining_test()
+    remove_at_end_test()
+    remove_at_index_test()
+    remove_by_value_test()
+
+    reversal_of_list_test()
+
     return
 
 if __name__ == '__main__':
